9308389171299cc4ccb3f9c3ee29e3d45bf7e2ff
tomaszpach <tomaszpach@icloud.com>
2018-11-26 20:05:34 +0100

    add submit handle and button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+
+// Material parts
+import Button from '@material-ui/core/Button';
+
+// Components
 import Names from './components/Names';
 
 class App extends Component {
@@ -22,13 +27,22 @@ class App extends Component {
         }
     }
 
+    handleSubmit(e) {
+        console.log(this.state);
+        e.preventDefault();
+    }
+
     render() {
 
         const {firstName, lastName} = this.state;
 
         return (
-            <form>
+            <form onSubmit={(e) => this.handleSubmit(e)}>
                 <Names firstName={firstName} lastName={lastName} onChange={(type, e) => this.handleChange(type, e)}/>
+
+                <Button type="submit" variant="contained" color="primary">
+                    Register
+                </Button>
             </form>
         );
     }
